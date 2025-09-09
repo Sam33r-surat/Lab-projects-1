86b2b8f3e9ef5ffd9baacf3744f066bfc5d1f949
Sam33r-surat <yreemas2409@gmail.com>
2025-09-09 18:09:03 +0530

    fixed spelling

--- 1_simple_interest.c
+++ 1_simple_interest.c
@@ -9,14 +9,14 @@ int main()
 {
     float p,r,t,i;
    
-    printf("Principle amount is: ");
+    printf("Principal amount is: ");
     scanf("%f",&p);
     printf("Interest rate is: ");
     scanf("%f",&r);
     printf("Time (in years) for which interest is to be calculated: ");
     scanf("%f",&t);
     i = ((p*r*t)/100);
-    printf("interest =%.2f\n",i);
+    printf("interest = %.2f\n",i);
     system("pause");
     return 0;
 }
